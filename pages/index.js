b3487d0db10066f7fe4c3ed8f60fc96ddd23491e
Lebeil <liedel75@gmail.com>
2021-09-06 17:06:52 +0200

    fix index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ products }) {
     );
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps() {
     const products = await fetch("https://fakestoreapi.com/products").then((res)=> res.json());
 
     return {
