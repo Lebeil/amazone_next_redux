a9ad9f563b3b134c2ec91dfaf09e31cb58c0b620
Lebeil <liedel75@gmail.com>
2021-09-08 16:17:15 +0200

    add Firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKZrZwxpMcl48bZK6qN4aK-hTscLC6-84",
+    authDomain: "e-next-863c3.firebaseapp.com",
+    projectId: "e-next-863c3",
+    storageBucket: "e-next-863c3.appspot.com",
+    messagingSenderId: "528765779445",
+    appId: "1:528765779445:web:a6c6f31319df215d8e74bc"
+};
